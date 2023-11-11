73399cfa0f1aa5a97e93b4f99775e5e7164fa01d
mantelloa <adam.mantello@student.unimelb.edu.au>
2023-11-11 14:00:16 +1100

    set up file sending api endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 
 const docx = require('docx');
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const fs = require('fs-extra');
 
 const app = express();
 const port = 3000;
@@ -9,27 +10,61 @@ const { Document, Packer, Paragraph, TextRun } = docx;
 
 app.use(cors());
 
-app.get('/report', async (req, res) => {
-    const doc = new Document();
 
-    const b64string = await Packer.toBase64String(doc);
-    
-    res.setHeader('Content-Disposition', 'attachment; filename=My Document.docx');
-    res.send(Buffer.from(b64string, 'base64'));
+app.get('/quote', async (req, res) => {
+    //const doc = createQuote();
+    const doc = await docx.patchDocument(fs.readFileSync("templates/quote.docx"), {
+        patches: {
+            client: {
+                type: docx.PatchType.PARAGRAPH,
+                children: [new TextRun("Aji Nav")]
+            },
+            address: {
+                type: docx.PatchType.PARAGRAPH,
+                children: [new TextRun("27 Kingfisher Gardens")]
+            }
+        }
+    })
+
+    fs.writeFileSync("finalquote.docx", doc);
+
+    res.setHeader('Content-Disposition', 'attachment; filename=finalquote.docx');
+    res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document');
+
+    res.sendFile("finalquote.docx", {root: __dirname}, (err) => {
+        if (err){
+            console.error('Error sending file:', err);
+            res.status(500).send('Internal Server Error');
+        } 
+        else {
+            console.log('File sent successfully!');
+            // Remove the file after sending it
+            fs.unlinkSync("finalquote.docx");
+        }
+    });
 });
+
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
 
 
-
-
 function createReport(){
-
-}
-
-function createQuote(){
     
 }
 
+function createQuote(){
+    return docx.patchDocument(fs.readFileSync("templates/quote.docx"), {
+        patches: {
+            client: {
+                type: docx.PatchType.PARAGRAPH,
+                children: [new TextRun("Aji Nav")]
+            },
+            address: {
+                type: docx.PatchType.PARAGRAPH,
+                children: [new TextRun("27 Kingfisher Gardens")]
+            }
+        }
+    });
+}
