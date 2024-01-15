6ceca85ee86ac78c937dbcf4b9984917ff070cb5
mantelloa <adam.mantello@student.unimelb.edu.au>
2024-01-15 16:06:10 +1100

    template for frontend website

--- frontend/script.js
+++ frontend/script.js
@@ -6,3 +6,25 @@ function generate(){
     console.log("Document has been sent");
     console.log(doc);
 }
+
+function getData(){
+    console.log("testing")
+    console.log(document.getElementById('job_address').value)
+    console.log(getCheckboxes('job_category'))
+    console.log(document.getElementById('water_damage_class').value)
+}
+
+function generateRoom(){
+
+}
+
+function getCheckboxes(checkbox_parent){
+    let checkedVals = []
+    let inputElements = document.getElementsByClassName(checkbox_parent)
+    for(let i=0; i < inputElements.length; i++){
+        if(inputElements[i].checked){
+            checkedVals.push(inputElements[i].value)
+        }
+    }
+    return checkedVals
+}
